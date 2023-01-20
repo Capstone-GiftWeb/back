3132d31a1311bb6ae9e88659bc6d8b03f2657a90
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-01-20 16:23:25 +0900

    Initial commit

---
+++ src/test/java/com/capstone/giftWeb/GiftWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.giftWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
