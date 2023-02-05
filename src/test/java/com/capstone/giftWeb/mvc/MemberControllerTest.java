c7fc813b74027f54a9335c18e5a33b75bc23327a
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-02-05 17:49:16 +0900

    각종 테스트 코드 추가

---
+++ src/test/java/com/capstone/giftWeb/mvc/MemberControllerTest.java
@@ -0,0 +1,44 @@
+package com.capstone.giftWeb.mvc;
+
+import com.capstone.giftWeb.Service.MemberService;
+import com.capstone.giftWeb.controller.MemberController;
+import jakarta.servlet.http.HttpSession;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MemberController.class)
+@AutoConfigureWebMvc
+public class MemberControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    MemberService memberService;
+
+    @Test
+    public void 로그인페이지입장() throws Exception {
+
+        mockMvc.perform(
+                get("/members/login")
+        ).andExpect(status().isOk());
+    }
+
+    @Test
+    public void 회원가입페이지입장() throws Exception{
+
+        mockMvc.perform(
+                get("/members/new")
+        ).andExpect(status().isOk());
+    }
+
+}
