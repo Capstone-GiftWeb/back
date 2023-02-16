ead00733272c488de0867f2600b608d75d42ca9c
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-02-16 12:52:16 +0900

    카카오톡 선물하기 랭킹 크롤링 쿠키 이용으로 1시간마다 한번씩만 받아오기

--- src/main/java/com/capstone/giftWeb/controller/HomeController.java
+++ src/main/java/com/capstone/giftWeb/controller/HomeController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,9 +22,20 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String home() {
-
-        List<String> list = homeService.makeGifts();
+    public String home(HttpServletResponse response,HttpServletRequest request) {
+        int flag=0;  //쿠키에 makeGifts가 없으면 크롤링, 있으면 크롤링X
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cok : cookies) {
+            if(cok.getName().equals("makeGifts"))
+                flag=1;
+        }
+        if(flag==0) {
+            List<String> list = homeService.makeGifts();
+            Cookie cookie = new Cookie("makeGifts", "yes");
+            cookie.setMaxAge(60 * 60);
+            cookie.setPath("/");
+            response.addCookie(cookie);
+        }
         return "home";
     }
 }
