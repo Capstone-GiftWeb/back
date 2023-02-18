8cfec93e637f670ff3a3ae08340a4960a4ec2b81
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-02-18 19:18:11 +0900

    이미지 lazy loading 문제 해결

--- src/main/java/com/capstone/giftWeb/Service/HomeService.java
+++ src/main/java/com/capstone/giftWeb/Service/HomeService.java
@@ -1,12 +1,14 @@
 package com.capstone.giftWeb.Service;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -15,16 +17,19 @@ import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Service
 public class HomeService {
+
     private WebDriver driver;
 
     private static final String url = "https://gift.kakao.com/ranking/best/delivery"; //카카오톡 선물하기 '많이 선물한' 랭킹
 
     public List<String> makeGifts() {
+
         System.setProperty("webdriver.chrome.driver", "./chromedriver/chromedriver.exe");
         //파이어폭스 드라이버 셋팅 (드라이버 설치한 경로 입력)
 
@@ -47,15 +52,16 @@ public class HomeService {
 
     private List<String> getDataList() {
         List<String> list = new ArrayList<>();
-
         try {
             driver.get(url);
-            driver.manage().timeouts().implicitlyWait(Duration.ofMinutes(5));
-            List<WebElement> elements = driver.findElements(By.cssSelector("#mArticle > app-pw-home > div > app-pw-best-ranking > div > app-pw-best-delivery > app-best-list > cu-infinite-scroll > div > div > ol > li > app-view-best-ranking-product"));
-
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+            Actions actions=new Actions(driver);
+            List<WebElement> elements = driver.findElements(By.cssSelector("app-view-best-ranking-product"));
             for (WebElement element : elements
             ) {
-                list.add(element.getAttribute("outerHTML").replace("<!---->", ""));
+                actions.moveToElement(element);
+                actions.perform();
+                list.add(element.getAttribute("outerHTML"));
             }
         } catch (Exception e) {
             e.printStackTrace();
