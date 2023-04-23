2635aafff35aef123fe0f768578f151033e1c7fb
goldsergeant <94740533+goldsergeant@users.noreply.github.com>
2023-04-23 20:20:42 +0900

    GiftSearchRepositoryTest

--- src/test/java/com/capstone/giftWeb/repository/GiftRepositoryTest.java
+++ src/test/java/com/capstone/giftWeb/repository/GiftRepositoryTest.java
@@ -14,7 +14,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@AutoConfigureTestDatabase
 class GiftRepositoryTest {
 
     @Autowired
